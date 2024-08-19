cd272f7bbbda133a7dc6f12b6e8d263c02457304
namcnab <nnekaxmcnab@gmail.com>
2024-08-18 20:37:29 -0700

    Defines the model for the plant db.

--- internal/model/plantsDB.go
+++ internal/model/plantsDB.go
@@ -1,7 +1,17 @@
 package model
 
-type GlossaryEntry struct {
-    ID         uint   `gorm:"primaryKey"`
-    Term       string
-    Definition string
+type Glossary struct {
+    ID         int   `gorm:"primaryKey"`
+    Term       string `gorm:"column:term"`
+    Definition string `gorm:"column:definition"`
+}
+
+// TableName method to explicitly set the table name
+func (Glossary) TableName() string {
+    return "glossary"
+}
+
+// set schema name
+func (Glossary) SchemaName() string {
+    return "public"
 }
